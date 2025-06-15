93d155fa66817722653491640527ed04893bf7a9
maheshroyce <maheswarr813@gmail.com>
2025-06-15 11:46:57 +0530

    updated the public folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +12,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_headers", // your headers file
+          dest: "."               // copies into dist/
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
